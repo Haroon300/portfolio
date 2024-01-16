03ee4097ef5c4a9f944c0d077d851d1e65dc38cd
Haroon300 <haroonhammad300@gmail.com>
2024-01-16 17:06:28 +0500

    Update education.js

--- src/app/component/education.js
+++ src/app/component/education.js
@@ -11,33 +11,38 @@ export const Education = () => {
               title="Bachelor"
               degree="Computer Science"
               institution="University Of Karachi UBIT"
+              year="2024"
             />
             <EducationItem
               title="Diploma of Associate Engineer"
               degree="Software Technology"
               institution="Aligarh Institute of Technology"
+              year="2021-2023"
             />
             <EducationItem
               title="Intermediate"
               degree="Pre Engineering"
               institution="Govt.Degree College Gulzar-E-Hijri"
+              year="2019-2020"
             />
             <EducationItem
               title="Matric"
               degree="Computer Science"
               institution="Gulf Schooling System"
+              year="2017-2018"
             />
           </div>        
     </div>
   )
 }
 
-const EducationItem = ({ title, degree, institution }) => {
+const EducationItem = ({ title, degree, institution, year }) => {
     return (
-      <div className="h-32 mt-4">
+      <div className="h-24 mt-4">
         <h1 className="text-xl font-semibold">{title}</h1>
         <h2 className="text-lg">{degree}</h2>
         <p>{institution}</p>
+        <p className='text-gray-500'>{year}</p>
       </div>
     );
   };
